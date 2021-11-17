adfb18c43a3910d67815c10b98a6d84703ba817a
Michael Stokesbary <mike@stokesbary.me>
2021-11-17 17:56:34 +0000

    forcing python version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -53,6 +53,7 @@ resource "azurerm_app_service" "example" {
   site_config {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
+    php_version              = "2.2"
   }
 
   app_settings = {
