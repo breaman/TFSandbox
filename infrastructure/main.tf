3fd90b3b5c74824193a0221b7b3f52d8a70471a3
Michael Stokesbary <mike@stokesbary.me>
2021-11-17 17:52:46 +0000

    force error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
+  name                = "app-service"
   location            = azurerm_resource_group.stokesops.location
   resource_group_name = azurerm_resource_group.stokesops.name
   app_service_plan_id = azurerm_app_service_plan.example.id
